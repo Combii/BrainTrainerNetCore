05bf2abcb5ab122375dd54c80d093512e909d6cd
borisgrunwald <borisgrunwald@gmail.com>
2019-11-29 12:19:51 +0100

    Added Login box

--- BrainTrainerSPA/src/BrainTrainer.js
+++ BrainTrainerSPA/src/BrainTrainer.js
@@ -1,73 +1,89 @@
-import React, {Component} from 'react';
-import NavBar from "./Component/NavBar/NavBar";
-import {Route, Switch, withRouter} from "react-router-dom";
-import Menu from "./Component/Menu/Menu";
-import {connect} from 'react-redux';
-import HighScoreList from "./Component/HighScore/HighScoreList";
-import asyncComponent from "./hoc/asyncComponent";
-import {loadLocalHighscores} from "./Utils/localHighscores";
+import React, { Component } from 'react';
+import NavBar from './Component/NavBar/NavBar';
+import { Route, Switch, withRouter } from 'react-router-dom';
+import Menu from './Component/Menu/Menu';
+import { connect } from 'react-redux';
+import HighScoreList from './Component/HighScore/HighScoreList';
+import asyncComponent from './hoc/asyncComponent';
+import { loadLocalHighscores } from './Utils/localHighscores';
+import Login from './Component/Login/Login';
 
-const asyncGameSection = asyncComponent(() => import("./Container/GameSection"));
+const asyncGameSection = asyncComponent(() =>
+  import('./Container/GameSection')
+);
 
 class BrainTrainer extends Component {
+  state = {
+    isLoginMode:false
+  };
 
+  toggleLoginMode = () => {
+      this.setState((prev,props) => ({
+          isLoginMode : !prev.isLoginMode
+      }))
+  }
 
-    difficultySelected = (difficulty) => {
-        this.props.onDifficultySelected(difficulty);
-        this.props.history.push('/braintrainer')
-    };
+  difficultySelected = difficulty => {
+    this.props.onDifficultySelected(difficulty);
+    this.props.history.push('/braintrainer');
+  };
 
+  viewHighscoresSelected = () => {
+    this.props.history.push('/highscores');
+  };
 
-    viewHighscoresSelected = () => {
-        this.props.history.push('/highscores')
-    };
+  render() {
+    const routes = [
+      {
+        path: '/',
+        component: Login,
+        componentProps: {
+            isLogin: this.state.isLoginMode,
+            toggleLogin : this.toggleLoginMode
+        }
+      },
+      {
+        path: '/menu',
+        component: Menu,
+        componentProps: {
+          diffSelected: this.difficultySelected,
+          highscoresSelected: this.viewHighscoresSelected
+        }
+      },
+      {
+        path: '/braintrainer',
+        component: asyncGameSection
+      },
+      {
+        path: '/highscores',
+        component: HighScoreList,
+        componentProps: {
+          scores: loadLocalHighscores()
+        }
+      }
+    ];
 
-    render() {
-
-        const routes = [
-            {
-                path:'/',
-                component:Menu,
-                componentProps:{
-                    diffSelected: this.difficultySelected,
-                    highscoresSelected: this.viewHighscoresSelected
-                }
-            },
-            {
-                path:'/braintrainer',
-                component: asyncGameSection
-            },
-            {
-                path: '/highscores',
-                component: HighScoreList,
-                componentProps: {
-                    scores: loadLocalHighscores()
-                }
-            },
-        ];
-
-        return (
-            <div>
-                <NavBar/>
-                <Switch>
-                    {routes.map(({path,component:C,componentProps}) => (
-                        <Route
-                            exact
-                            key={path}
-                            path={path}
-                            render={(props) => <C {...props} {...componentProps}/>}
-                        />
-                    ))}
-                </Switch>
-            </div>
-        )
-    }
-
+    return (
+      <div>
+        <NavBar />
+        <Switch>
+          {routes.map(({ path, component: C, componentProps }) => (
+            <Route
+              exact
+              key={path}
+              path={path}
+              render={props => <C {...props} {...componentProps} />}
+            />
+          ))}
+        </Switch>
+      </div>
+    );
+  }
 }
 
 const mapDispatchToProps = dispatch => ({
-    onDifficultySelected : (difficulty) => dispatch({difficulty,type:'SET_DIFFICULTY'})
+  onDifficultySelected: difficulty =>
+    dispatch({ difficulty, type: 'SET_DIFFICULTY' })
 });
 
-
-export default withRouter(connect(null,mapDispatchToProps)(BrainTrainer));
+export default withRouter(connect(null, mapDispatchToProps)(BrainTrainer));
---
+++ BrainTrainerSPA/src/Component/Login/Login.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import './Login.css';
+import Button from '../../UI/Button/Button';
+
+const Login = ({ isLogin, toggleLogin }) => {
+  return (
+    <div className='root-container'>
+      <div className='box-container'>
+        <div className='inner-container'>
+          <div className='header'>{isLogin ? 'Login' : 'Register'}</div>
+          <div className='box'>
+            <div className='input-group'>
+              <label htmlFor='username'>Username</label>
+              <input
+                type='text'
+                name='username'
+                className='login-input'
+                placeholder='Username'
+              />
+            </div>
+
+            <div className='input-group'>
+              <label htmlFor='password'>Password</label>
+              <input
+                type='password'
+                name='password'
+                className='login-input'
+                placeholder='Password'
+              />
+            </div>
+
+            <button className='already-a-member' onClick={toggleLogin}>
+              {isLogin ?  'Not a member yet?' : 'Already a member?'}
+            </button>
+
+            <button type='button' className='login-btn'>
+              {isLogin ? 'Login' : 'Sign up'}
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
